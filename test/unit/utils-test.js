dfa7a0766b5f8dfe7f7947c6a0e664cf431bda50
Rafael Verger <rafael@rafaelverger.com.br>
2014-03-24 18:42:36 -0300

    Moving buildXML methods from maxipago to utils module

--- test/unit/utils-test.js
+++ test/unit/utils-test.js
@@ -1,4 +1,6 @@
-var utils = require('../../lib/utils');
+var proxyquire = require('proxyquire'),
+    sinon = require('sinon'),
+    models = require('../../lib/models');
 
 exports.testFormatObject = function(test) {
 
@@ -30,10 +32,51 @@ exports.testFormatObject = function(test) {
   test.notEqual(Object.keys(to_format), Object.keys(strict_obj));
   test.notEqual(to_format, expected);
 
-  var result = utils.formatObject(to_format, strict_obj);
+  var result = require('../../lib/utils').formatObject(to_format, strict_obj);
 
   test.deepEqual(result, expected);
   test.deepEqual(Object.keys(result), Object.keys(strict_obj));
 
   test.done();
+};
+
+exports.testBuildAddCustomerXML = function(test) {
+  var sandbox = sinon.sandbox.create();
+
+  var buildObj = sandbox.spy();
+  var fakeBuilder = sandbox.spy(function(){
+    return {buildObject: buildObj};
+  });
+
+  var utils = proxyquire('../../lib/utils', {
+    'xml2js': {Builder: fakeBuilder},
+  });
+
+  var formattedObj = 'formattedObj';
+  var formatObject = sandbox.stub(utils, 'formatObject');
+  formatObject.returns(formattedObj);
+
+  var data = {'data': 'data'},
+      auth = {'auth': 'auth'},
+      xmlOpts = {'opt': 'opt'};
+
+  utils.buildAddCustomerXML(data, auth, xmlOpts);
+
+  test.ok(formatObject.calledOnce);
+  test.ok(formatObject.calledWithExactly(data, models.addCustomer));
+
+  test.ok(fakeBuilder.calledOnce);
+  test.ok(fakeBuilder.calledWithExactly(xmlOpts));
+
+  test.ok(buildObj.calledOnce);
+  test.ok(buildObj.calledWithExactly({
+    'api-request': {
+      verification: auth,
+      command: 'add-consumer',
+      request: formattedObj
+    }
+  }));
+
+  test.done();
+  sandbox.restore();
 };
